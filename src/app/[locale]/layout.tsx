b8361dc98f68a6efb314adb6134d711212e008a7
kabaskill <oguzkabasakal@gmail.com>
2024-11-13 23:29:50 +0100

    analytics update v8

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import Loading from "@/components/Common/Loading";
-import {  ConsentBanner } from "@/components/Google/Analytics";
+import { ConsentBanner } from "@/components/Google/Analytics";
 
 export default function SubLayout({
   children,
@@ -16,8 +16,8 @@ export default function SubLayout({
 }) {
   return (
     <I18nProviderClient locale={params.locale} fallback={<Loading />}>
-      <ConsentBanner />
       <main className="size-full">
+        <ConsentBanner />
         <Header />
         {children}
         <ScrollToTop />
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -13,7 +13,7 @@ export default async function Contact() {
       <div className={cn("mb-24 flex flex-wrap")}>
         <ContactCard />
         {/* <MapComponent /> */}
-        <div className="w-full px-4 pb-4">
+        <div className="w-3/5 px-4 pb-4">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2580.169804862286!2d8.844841716017683!3d49.70760484848979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd607196f1b0cb%3A0xba05bbb5a15f044e!2sFormenwerkstatt+GmbH!5e0!3m2!1sde!2sde!4v1482094744213"
             width="100%"
