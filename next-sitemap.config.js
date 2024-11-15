6316bc1b26552c135ef28258fee1ff8e5400eb86
kabaskill <oguzkabasakal@gmail.com>
2024-11-15 09:35:47 +0100

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,38 @@
 module.exports = {
-  siteUrl: "https://formenwerkstatt.de",
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
+  siteUrl: "https://formenwerkstatt.de", // Your website URL
+  generateRobotsTxt: true, // Generate a robots.txt file
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/404", "/500"], // Exclude non-essential pages
+  additionalPaths: async (config) => {
+    // This function will dynamically add paths
+    const dynamicPaths = await fetchDynamicPaths();
+    return dynamicPaths.map((path) => ({
+      loc: path, // Full URL of the dynamic page
+      lastmod: new Date().toISOString(),
+    }));
+  },
+};
+
+const fetchDynamicPaths = async () => {
+  const dynamicData = [
+    "/contact",
+    "/jobs",
+    "/about",
+    "/services/beratung-und-design",
+    "/services/werkzeugherstellung",
+    "/services/abmusterung-und-kleinserienproduktion",
+    "/services/fraesen",
+    "/services/drehen",
+    "/services/senkerodieren",
+    "/services/drahterodieren",
+    "/services/laserschweissen",
+    "/services/lasergravieren",
+    "/services/hochglanzpolieren",
+    "/services/flachschleifen",
+    "/services/optimierung",
+    "/services/uberholung",
+    "/services/werkzeug-reparatur",
+  ];
+  return dynamicData.map((slug) => `https://formenwerkstatt.de/${slug}`);
 };
