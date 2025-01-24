cbb188bb3d19ada25b86edaf19f1fe04ed1dafd5
kabaskill <oguzkabasakal@gmail.com>
2025-01-24 14:12:59 +0100

    firebase and get method added plus styling for shop

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,15 +7,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCnXX9_fDMLxzdYY6OtSUoh8mZE9nCaozg",
-  authDomain: "fw-shop-28a91.firebaseapp.com",
-  projectId: "fw-shop-28a91",
-  storageBucket: "fw-shop-28a91.firebasestorage.app",
-  messagingSenderId: "36643113938",
-  appId: "1:36643113938:web:023ee42dd76519ed03160b",
-  measurementId: "G-BL5QFHK4JX",
-};
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}');
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
